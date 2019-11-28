b9dd0164e1e428113cef5114cdf7025b2bf42080
Steve Soltys <steve@stevesoltys.com>
2019-11-28 13:03:03 -0500

    Allow zero experience in skill constructor precondition

--- src/main/java/com/github/michaelbull/rs/hiscores/Skill.java
+++ src/main/java/com/github/michaelbull/rs/hiscores/Skill.java
@@ -73,7 +73,7 @@ public final class Skill {
 	public Skill(int rank, int level, long experience) {
 		Preconditions.checkArgument(rank == -1 || rank > 0, "Rank must be either -1 (unranked) or positive.");
 		Preconditions.checkArgument(level >= 0, "Level must be either 0 (unranked) or positive.");
-		Preconditions.checkArgument(experience == -1 || experience > 0, "Experience must be either -1 (unranked) or positive.");
+		Preconditions.checkArgument(experience >= -1, "Experience must be greater than or equal to -1.");
 		this.rank = rank;
 		this.level = level;
 		this.experience = experience;
